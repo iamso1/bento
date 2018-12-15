71af177f0dd9e23b6b1f5f003360fa0d40cd210a
Hsinhan <bmhiamso1@gmail.com>
2018-12-16 05:42:29 +0800

    實作判斷邏輯
    1. 實作user的方法判斷邏輯
    2. 規劃admin的方法

--- handlers/handle-admin-flow.js
+++ handlers/handle-admin-flow.js
@@ -1,16 +1,27 @@
 const { LineHandler } = require('bottender');
 
 //TODO 實作各種功能判斷以及回應
+
 //查詢訂單
 const isGetOrders = context => {
   return true;
 };
 
 const handleGetOrders = context => {
+  context.replyText('admin getOrders');
+};
+
+//查詢送貨資訊
+const isGetShipping = context => {
+  return false;
+};
+
+const handleGetShipping = context => {
   context.replyText('You click QueryOrders');
 };
 
 module.exports = new LineHandler()
   .on(isGetOrders, handleGetOrders)
+  .on(isGetShipping, handleGetShipping)
   .onEvent(context => console.log('Uncaught event:', context.event.rawEvent))
   .build();
--- handlers/handle-user-flow.js
+++ handlers/handle-user-flow.js
@@ -1,16 +1,48 @@
 const { LineHandler } = require('bottender');
 
+const preprocess = context => {
+  const { groupId, userId } = context.event.source;
+  const { text } = context.event.message;
+  return {
+    groupId,
+    userId,
+    text,
+  };
+};
+
 //TODO 實作各種功能判斷以及回應
+//產生訂單
+const isGenOrders = context => {
+  const { groupId, userId, text } = preprocess(context);
+  console.log(groupId, userId, text);
+  return true;
+};
+
+const handleGenOrders = context => {
+  context.replyText('user GenOrders ');
+};
+
 //查詢訂單
 const isGetOrders = context => {
-  return true;
+  return false;
 };
 
 const handleGetOrders = context => {
   context.replyText('You click QueryOrders');
 };
 
+//取消訂單
+const isCancelOrders = context => {
+  return false;
+};
+
+const handleCancelOrders = context => {
+  context.replyText('You click QueryOrders');
+};
+
 module.exports = new LineHandler()
+  .on(isGenOrders, handleGenOrders)
   .on(isGetOrders, handleGetOrders)
+  .on(isCancelOrders, handleCancelOrders)
   .onEvent(context => console.log('Uncaught event:', context.event.rawEvent))
   .build();
